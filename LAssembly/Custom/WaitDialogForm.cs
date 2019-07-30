4dd69e01ae140e039497c5f158ed7b496d0ec531
MK <MK@LIANG>
2019-07-30 13:44:58 +0800

    WaitDialogForm

---
+++ LAssembly/Custom/WaitDialogForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace MR.LTools.Custom
+{
+   public static class WaitDialogForm
+    {
+        static Thread load = null;
+        public static void ShowWaitdialogFrom(string strMess, Control control=null)
+        {
+            Thread load = new Thread((ThreadStart)delegate
+            {
+                frmLoading frmLoading = new frmLoading(control);
+                frmLoading.SetText(strMess);
+                if (control == null)
+                {
+                    frmLoading.StartPosition = FormStartPosition.CenterScreen;
+                }
+                frmLoading.ShowDialog();
+            });
+            load.Start();
+        }
+        public static void CloseWaitdialogFrom()
+        {
+            load.Abort();
+        }
+    }
+}
