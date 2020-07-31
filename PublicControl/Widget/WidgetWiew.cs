4c5e274ecba7d894e77591582fa955517ba4f2cb
Liang <liangwanzhuang@bjgoodwill.com>
2020-07-31 14:37:35 +0800

    2

---
+++ PublicControl/Widget/WidgetWiew.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using PublicControl.Controls;
+
+namespace PublicControl.Widget
+{
+    public partial class WidgetWiew : UserControl
+    {
+        public WidgetWiew()
+        {
+            InitializeComponent();
+        }
+        public virtual  void WidgetRefresh()
+        {
+        
+        }
+        public virtual bool IsClose()
+        {
+            return true;
+        }
+    }
+}
